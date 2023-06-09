89fd1e840c832eaa3da372ff20428a02731b1dda
riyaG30 <123980537+riyaG30@users.noreply.github.com>
2023-06-09 18:50:20 +0530

    Create TopView.cpp

---
+++ Binary Tree/TopView.cpp
@@ -0,0 +1,29 @@
+ vector<int> topView(Node *root)
+    {
+        //Your code here
+        vector<int> ans;
+        map<int,int> mpp; // for storing the line number and first node data from top
+        queue<pair<Node*,int>> q ; // to store all node and their line number
+        q.push({root,0});
+        while(!q.empty())
+        {
+            auto it = q.front();
+            q.pop();
+            Node* node = it.first;
+            int line = it.second;
+            if(mpp.find(line) == mpp.end())
+            mpp[line] = node->data;
+            if(node->left)
+            q.push({node->left,line-1});
+            if(node->right)
+            q.push({node->right,line+1});
+            
+        }
+        for(auto it :mpp)
+        {
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+
+};
